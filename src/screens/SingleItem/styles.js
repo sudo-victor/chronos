83c7c4024b5a08f654a7898b6be264b8191b4675
Victor de Souto <victor.souto@hotmail.com>
2020-07-01 09:42:19 -0400

    style: change background

--- src/screens/SingleItem/styles.js
+++ src/screens/SingleItem/styles.js
@@ -1,4 +1,9 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+    flex: 1;
+
+    background-color: #fafafa;
+`;
+
 export const Text = styled.Text``;
